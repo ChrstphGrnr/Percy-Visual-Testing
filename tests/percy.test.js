5633b2c12a0def922020eb8b7250e2e9f22d51ac
ChrstphGrnr <cgruener@quantummetric.com>
2020-04-10 16:33:59 -0600

    add percySnapshot in a test

---
+++ tests/percy.test.js
@@ -0,0 +1,13 @@
+import { Selector } from 'testcafe'
+import percySnapshot from '@percy/testcafe'
+
+//prettier ignore
+
+fixture`Visual Regression Test with Percy`
+    .page`https://www.example.com`
+
+
+test('Percy Demo - Example Page', async t => {
+    await t.wait(1000)
+    await percySnapshot(t, "Example Page")
+})
